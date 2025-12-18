95747d1d978ddaa9e5600715e1cc3fc4717ab741
H <huangsiyan1012@163.com>
2025-12-18 13:59:37 +0800

    脚手架目录结构

--- react_staging/01_src_脚手架自带文件结构介绍/src/index.js
+++ react_staging/01_src_脚手架自带文件结构介绍/src/index.js
@@ -2,7 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 // 用于记录文件性能
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,3 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
