bb3bf8fc75b836d7a57e1619e5f18652c7189c23
H <huangsiyan1012@163.com>
2025-12-24 14:47:13 +0800

    嵌套路由基本使用

--- react_staging/src/pages/Home/index.jsx
+++ react_staging/src/pages/Home/index.jsx
@@ -1,7 +1,31 @@
 import { Component } from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import MyNavLink from "../../components/MyNavLink";
+import Message from "./Message";
+import News from "./News";
 
 export default class Home extends Component {
   render() {
-    return <h3>我是Home的内容</h3>;
+    return (
+      <div>
+        <h3>我是Home的内容</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              <MyNavLink to="/home/news">News</MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to="/home/message">Message</MyNavLink>
+            </li>
+          </ul>
+          <div>
+            <Switch>
+              <Route path="/home/news" component={News}></Route>
+              <Route path="/home/message" component={Message}></Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
