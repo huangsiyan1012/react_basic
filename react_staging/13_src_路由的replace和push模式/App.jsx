844fc5ca34d9fe55184d154c9adffe27e2198519
H <huangsiyan1012@163.com>
2025-12-25 11:06:01 +0800

    路由的replace和push模式

---
+++ react_staging/13_src_路由的replace和push模式/App.jsx
@@ -0,0 +1,51 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import Header from "./components/Header";
+import MyNavLink from "./components/MyNavLink";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="root">
+        <div>
+          <div className="row">
+            <Header></Header>
+          </div>
+          <div className="row">
+            <div className="col-xs-2 col-xs-offset-2">
+              <div className="list-group">
+                {/* 原生html中，使用<a>标签跳转到不同页面 */}
+                {/*  <a className="list-group-item" href="./about.html">
+                  About
+                </a>
+                <a className="list-group-item active" href="./home.html">
+                  Home
+                </a> */}
+
+                {/* 在React中靠路由链接实现切换组件 - 编写路由链接 */}
+                <MyNavLink to="/home">Home</MyNavLink>
+                <MyNavLink to="/about">About</MyNavLink>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <div className="panel">
+                <div className="panel-body">
+                  {/* 注册路由 */}
+                  <Switch>
+                    <Route path="/about" component={About}></Route>
+                    <Route path="/home" component={Home}></Route>
+                    <Redirect to="/home"></Redirect>
+                  </Switch>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
