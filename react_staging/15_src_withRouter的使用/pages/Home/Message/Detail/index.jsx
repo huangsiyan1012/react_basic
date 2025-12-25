94db5881bead8f6c2d8e1289741d0f47a0970072
H <huangsiyan1012@163.com>
2025-12-25 13:41:34 +0800

    withRouter的使用

---
+++ react_staging/15_src_withRouter的使用/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,27 @@
+// 用于将urlencode形式的字符串转换成对象形式,这个库可以两个状态互转
+// import qs from "querystring";
+import { Component } from "react";
+
+const DetailDate = [
+  { id: "01", content: "MESSAGE1" },
+  { id: "02", content: "MESSAGE2" },
+  { id: "03", content: "MESSAGE3" },
+];
+export default class Detail extends Component {
+  render() {
+    console.log(this.props);
+    // 接收params参数
+    const { id, title } = this.props.match.params;
+
+    const findResult = DetailDate.find((item) => item.id === id) || {};
+    return (
+      <div>
+        <ul>
+          <li>ID:{id}</li>
+          <li>TITLE:{title}</li>
+          <li>CONTENT:{findResult.content}</li>
+        </ul>
+      </div>
+    );
+  }
+}
